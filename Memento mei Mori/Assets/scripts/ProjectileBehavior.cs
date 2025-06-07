6023edac8b427526edefc3eaceee3d4ea7d0cd9f
Pikaplejer1 <pendykmarcel@gmail.com>
2025-06-08 00:19:24 +0200

    fixed something

--- Memento mei Mori/Assets/scripts/ProjectileBehavior.cs
+++ Memento mei Mori/Assets/scripts/ProjectileBehavior.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class ProjectileBehavior : MonoBehaviour
@@ -12,6 +13,17 @@ public class ProjectileBehavior : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         collision.gameObject.GetComponent<Player>()?.getDamage();
+
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Border")
+        {
+            Destroy(gameObject);
+        }
+        else destroyAfterTime(2.5f);
+    }
+    
+    IEnumerator destroyAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
         Destroy(gameObject);
     }
 }
